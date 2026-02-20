4a577213d7245afed6f24d8ccb62823e7d289e1f
Atik <atik0585@gmail.com>
2026-02-20 14:15:33 -0500

    Stable single-device version before multi-device architecture

---
+++ SimStreamerWebRTC/SimWebRTCStreamer/SimWebRTCStreamer/SimWebRTCStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimWebRTCStreamerApp.swift
+//  SimWebRTCStreamer
+//
+//  Created by Rohini Ramalingam on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SimWebRTCStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
